85ffeaebb7829809f4122e82f8842778b792aa08
nxphi <xuanphi001@e.ntu.edu.sg>
2016-06-08 17:24:45 +0800

    I just update everything

--- learnJS/main.js
+++ learnJS/main.js
@@ -25,12 +25,13 @@ var changeBackground = {
 var changeContent = {
     personalInit: function () {
         var division = document.getElementById("content");
-        while (division.hasChildNodes()){
+        while (division.hasChildNodes()) {
             division.removeChild(division.lastChild);
         }
         function makeBr() {
             return document.createElement("br");
         }
+
         // form making
         var form = document.createElement("form");
         form.style.backgroundColor = "white";
@@ -69,11 +70,11 @@ var changeContent = {
 
         var genderText = document.createTextNode("Gender: ");
         var gender = [];
-        for (var i = 0; i < 2; i++){
+        for (var i = 0; i < 2; i++) {
             var gen = document.createElement("input");
             gen.type = "radio";
-            gen.value = (i == 0)?"male":"female";
-            gen.innerHTML = (i == 0)?"Male":"Female";
+            gen.value = (i == 0) ? "male" : "female";
+            gen.innerHTML = (i == 0) ? "Male" : "Female";
             gender.concat(gen);
         }
 
@@ -102,33 +103,43 @@ var changeContent = {
         // finish formaking
         division.appendChild(form);
     },
-    
-    statusInit: {
-        
-    }
+
+    statusInit: {}
 };
 
 function curbChangeColor(text) {
     var style = text.style;
-    if (text.innerHTML == "5 cm"){
+    if (text.innerHTML == " 5 cm") {
         style.backgroundColor = "green";
         style.color = "black";
     }
-    else if (text.innerHTML == "10 cm"){
+    else if (text.innerHTML == "10 cm") {
         style.backgroundColor = "yellow";
         style.color = "black";
     }
-    else if (text.innerHTML == "15 cm"){
+    else if (text.innerHTML == "15 cm") {
         style.backgroundColor = "red";
         style.color = "white";
+    }
+    else if (text.innerHTML == "none") {
+        style.backgroundColor = "lightblue";
+        style.color = "black";
+    }
+    else if (text.innerHTML == "unknown") {
+        style.backgroundColor = "saddlebrown";
+        style.color = "white";
+    }
+    else {
+        console.log("Error with curb input when change style");
+        console.log(text.innerHTML);
     }
 }
 
 function curbChangeValue(text) {
     var val = parseInt(text.innerHTML.substring(0, text.innerHTML.indexOf('c')));
     var newVal = val;
-    if (val == 15){
-        newVal =5;
+    if (val == 15) {
+        newVal = 5;
     }
     else {
         newVal += 5;
@@ -138,11 +149,12 @@ function curbChangeValue(text) {
 }
 
 var realtime = {
+    activated : false,
     init: function () {
         this.div = document.getElementById("display");
         this.table = document.createElement("table");
         this.table.align = "center";
-        
+
         var caption = document.createElement("caption");
         caption.id = "caption";
         caption.innerHTML = "RealTime demo";
@@ -161,7 +173,7 @@ var realtime = {
         this.statusVal.className = "value";
         this.statusVal.innerHTML = "Burst";
         this.statusRow.appendChild(this.statusVal);
-        
+
         // Curb length row
         this.curbRow = document.createElement("tr");
         this.curbRow.id = "curbRow";
@@ -171,12 +183,9 @@ var realtime = {
         this.curbRow.appendChild(curbLabel);
         this.curbVal = document.createElement("td");
         this.curbVal.className = "value";
-        this.curbVal.innerHTML = "5 cm"; //  change according to....
+        this.curbVal.innerHTML = " 5 cm"; //  change according to....
         // need to add the default style and change of style
         //noinspection JSValidateTypes
-        this.curbVal.addEventListener("click", function () {
-            curbChangeValue(this);
-        });
         this.curbRow.appendChild(this.curbVal);
 
 
@@ -209,14 +218,8 @@ var realtime = {
         this.logTd.colSpan = "2";
         this.log = document.createElement("textarea");
         this.log.className = "log";
-        this.log.value = "asdjkasldjklajklw\n" +
-            "akwjdawjdlkawjdlakwdjwalk fwaidjawidjawd\n" +
-            "aslkdjakljda\n" +
-            "dkajdjwiaofjawj \nadawjddawdjkawdjawkldj\n" +
-            "dawjdkawjdjaiodjawoidjawiodjawio\n" +
-            "adklajwkdjawajwk fljawfioaw\n" +
-            "awkjdawjdlkawjdklawjdklawjdkawljdkawjdkalwjdawljdalwkjd\n" +
-            "wankaw jawja wjowj waopjf \n";
+        this.log.value = "Log testing\n";
+
         this.log.rows = "10";
         this.log.cols = "60";
         this.log.disabled = "disabled";
@@ -237,41 +240,124 @@ var realtime = {
         //add to the div
         this.div.appendChild(this.table);
         this.addStyle();
+        this.activated = true;
     },
 
     addStyle: function () {
         var table = document.getElementsByTagName("table")[0];// since we have 2 table
         table.style.border = "1px solid black";
         table.style.borderCollapse = "collapse";
-        table.style.fontSize =  "150%";
+        table.style.fontSize = "150%";
         table.style.width = "500px";
 
         var td = document.getElementsByTagName("td");
-        for (var i = 0; i < td.length; i++){
+        for (var i = 0; i < td.length; i++) {
             td.item(i).style.border = "1px solid black";
             td.item(i).style.borderCollapse = "collapse";
             td.item(i).style.fontSize = "150%";
         }
 
         var tdLabel = document.getElementsByClassName("label");
-        for (var i = 0; i < tdLabel.length; i++){
+        for (var i = 0; i < tdLabel.length; i++) {
             tdLabel.item(i).style.backgroundColor = "#210ebd";
             tdLabel.item(i).style.color = "aliceblue";
         }
         var tdValue = document.getElementsByClassName("value");
-        for (var i = 0; i < tdValue.length; i++){
+        for (var i = 0; i < tdValue.length; i++) {
             tdValue.item(i).style.backgroundColor = "white";
             tdValue.item(i).style.color = "black";
             tdValue.item(i).style.textAlign = "center";
         }
         /*
-        var log = document.getElementsByClassName("log");
-        log.item(0).style.margin = "0";
-        log.item(0).style.fontSize = "75%";
-        log.item(0).style.backgroundColor = "white";
-        */
+         var log = document.getElementsByClassName("log");
+         log.item(0).style.margin = "0";
+         log.item(0).style.fontSize = "75%";
+         log.item(0).style.backgroundColor = "white";
+         */
+    },
+
+    updateStatus: function (burstOrNot) {
+        if (burstOrNot) {
+            this.statusVal.innerHTML = "Burst";
+        }
+        else {
+            this.statusVal.innerHTML = "Measurement";
+        }
+    },
+
+    updateCurb: function (val) {
+        switch (val) {
+            case " 5":
+            case "10":
+            case "15":
+                this.curbVal.innerHTML = val + " cm";
+                break;
+            case "none":
+            case "unknown":
+                this.curbVal.innerHTML = val;
+                break;
+            default:
+                this.curbVal.innerHTML = "Error";
+        }
+        curbChangeColor(this.curbVal);
+    },
+
+    updateDist: function (num) {
+        this.distVal.innerHTML = num + " m";
+    },
+
+    updateLog: function (info) {
+        this.log.value += info + "\n";
+    },
+
+    showUpdateRealTime: function (burst, curbVal, distVal) {
+        // -- need to check if realtime mode activated or not
+        if (this.activated == false) {
+            console.log("Error realtime mode not activated at show update");
+        }
+
+        //need to update the status curb, distance and log
+        var statusBurst;// true or false
+        var curb;// string
+        var dist;
+
+        statusBurst = burst;
+        //curb = curbVal;
+        if (curbVal == "5"){
+            curb = " 5";
+        }
+        else curb = curbVal;
+        console.log(curb);
+        dist = distVal;
+        // code for update the status
+        realtime.updateStatus(statusBurst);
+        realtime.updateCurb(curb);
+        realtime.updateDist(dist);
+
+        // log output
+        var time = new Date();
+        var log = time.toTimeString().substring(0, 8) + "--";
+        if (curb == "none") {
+            log += "found nothing";
+        }
+        else if (curb == "unknown") {
+            log += "found unknown at " + dist + " m";
+        }
+        else {
+            //when found curb
+            log += "found curb " + curb + " at " + dist + " m";
+        }
+        realtime.updateLog(log)
+
     }
-
-    
-
 };
+
+function getTime() {
+    var time1 = new Date().getTime();
+    var time2 = new Date().getTimezoneOffset();
+    var time3 = new Date().getUTCHours();
+    var time4 = new Date();
+    var output = document.getElementById("time");
+    output.innerHTML = time1 + "<br>" + time2 + "<br>" + time3 + "<br>" +
+        time4.toTimeString().substring(0, 8);
+}
