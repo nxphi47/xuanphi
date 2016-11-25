809784e343eb9065a2992ef25d62f78d85009810
nxphi47 <nxphi47@gmail.com>
2016-11-25 17:42:27 +0800

    update

--- javaAlgo/src/main/Main.java
+++ javaAlgo/src/main/Main.java
@@ -3,19 +3,112 @@ package main;
 import dataStructure.*;
 
 import java.util.*;
+import java.util.regex.Matcher;
 
 public class Main {
 	public static long hanoiTimes = 0;
 
-
-	public static void main(String[] args) throws Exception {
-//		ConnectedCellGrid cellGrid = new ConnectedCellGrid(true);
-		Pairs pairs = new Pairs(false);
+	public static double funcRungeKuta(double x, double y){
+		return 1/x + 2 * y;
 	}
 
-	public static int appointRegion(int[][] mainArr, int[][] regArr, int[][] marker, int i, int j){
+	public static double funcSys1(double x, double y1, double y2){
 
-		return 0;
+		return y2;
+	}
+	public static double funcSys2(double x, double y1, double y2){
+
+//		return 1 - 3 * y1 / x + 2 * y2 / x;
+//		return (2 * x * y2 + 1 - y1) / x;
+//		return (y2 - x * y1 + 3 * x) / (x * 2);
+		return (2 - x * y2 + 4 * y1) / x;
+	}
+
+
+	public static double eulerMultiple(double x, double y1, double y2, double h, double iteration){
+		double nextY1;
+		double nextY2;
+		for (int i = 0; i < iteration; i ++){
+			System.out.printf("%d%12f%12f%12f\n", i, x, y1, y2);
+			nextY1 = y1 + h * funcSys1(x, y1, y2);
+			nextY2 = y2 + h * funcSys2(x, y1, y2);
+			y1 = nextY1;
+			y2 = nextY2;
+			x += h;
+		}
+		return y1;
+	}
+
+	public static double eulerImprovedMultiple(double x, double y1, double y2, double h, double iteration){
+		double nextY1;
+		double nextY2;
+		double a1, a2;
+		double b1, b2;
+		for (int i = 0; i < iteration; i ++){
+			a1 = h * funcSys1(x, y1, y2);
+			a2 = h * funcSys2(x, y1, y2);
+			b1 = h * funcSys1(x + h, y1 + a1, y2 + a2);
+			b2 = h * funcSys2(x + h, y1 + a1, y2 + a2);
+			System.out.printf("%d %12f %12f %12f %12f %12f %12f %12f\n", i, x, y1, y2, a1, a2, b1, b2);
+			nextY1 = y1 + 0.5 * (a1 + b1);
+			nextY2 = y2 + 0.5 * (a2 + b2);
+			y1 = nextY1;
+			y2 = nextY2;
+			x += h;
+		}
+		return y1;
+	}
+
+	public static double rungeKutaMultiple(double x, double y1, double y2, double h, double iteration){
+		double nextY1;
+		double nextY2;
+		double a1, a2;
+		double b1, b2;
+		double c1, c2;
+		double d1, d2;
+		for (int i = 0; i < iteration; i ++){
+			a1 = h * funcSys1(x, y1, y2);
+			a2 = h * funcSys2(x, y1, y2);
+			b1 = h * funcSys1(x + h / 2, y1 + a1 / 2, y2 + a2 / 2);
+			b2 = h * funcSys2(x + h / 2, y1 + a1 / 2, y2 + a2 / 2);
+			c1 = h * funcSys1(x + h / 2, y1 + b1 / 2, y2 + b2 / 2);
+			c2 = h * funcSys2(x + h / 2, y1 + b1 / 2, y2 + b2 / 2);
+			d1 = h * funcSys1(x + h, y1 + c1, y2 + c2);
+			d2 = h * funcSys2(x + h, y1 + c1, y2 + c2);
+			System.out.printf("%d %12f %12f %12f %12f %12f %12f %12f %12f %12f %12f %12f\n", i, x, y1, y2, a1, a2, b1, b2, c1, c2, d1, d2);
+			nextY1 = y1 + (a1 + 2 * b1 + 2 * c1 + d1) / 6;
+			nextY2 = y2 + (a2 + 2 * b2 + 2 * c2 + d2) / 6;
+			y1 = nextY1;
+			y2 = nextY2;
+			x += h;
+		}
+		return y1;
+	}
+
+
+
+	public static double rungeKutaSingle(double x, double y, double h, int iteration){
+//		double x = 1;
+//		double h = 0.1;
+//		double y = 4;
+		double a,b,c,d;
+		System.out.printf("xn ---- yn ------ a ------- b ------c -------d\n");
+		for (int i = 0; i < iteration; i++){
+			a = h * funcRungeKuta(x, y);
+			b = h * funcRungeKuta(x + h / 2, y + a / 2);
+			c = h * funcRungeKuta(x + h / 2, y + b / 2);
+			d = h * funcRungeKuta(x + h, y + c);
+			System.out.printf("%12f%12f%12f%12f%12f%12f\n", x, y, a, b, c, d);
+			y += (a + 2 * b + 2 * c + d)/6;
+			x += h;
+		}
+		return y;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+
+		rungeKutaMultiple(3, 3, 2, 0.1, 3);
 	}
 
 
